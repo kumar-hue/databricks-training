dea5917c08078fb312b1cbd2e29c2a8422ba669b
kumar-hue <ch.v.n.kumar143@gmail.com>
2026-05-08 22:52:30 +0530

    Create questions.sql

---
+++ week1/day1/questions.sql
@@ -0,0 +1,14 @@
+1. Select all columns from Employee table
+
+SELECT * FROM Employee;
+
+-- 2. Select name and salary columns
+
+SELECT name, salary
+FROM Employee;
+
+-- 3. Select employees older than 30
+
+SELECT *
+FROM Employee
+WHERE age > 30;
