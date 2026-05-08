bc4b007f458a2e705cc872ab58d2f756e7ff55e4
kumar-hue <ch.v.n.kumar143@gmail.com>
2026-05-08 23:20:06 +0530

    Update questions.sql
    Added day1 questions

--- week1/day1/questions.sql
+++ week1/day1/questions.sql
@@ -1,5 +1,15 @@
-1. Select all columns from Employee tabl
+1. Select all columns from Employee table
 
 2. Select name and salary columns
 
 3. Select employees older than 30
+
+4. select employees whose names start with 'j'.
+
+5.select employees whose names exactly 9 characters long.
+
+6.select employees hired in the year 2020.
+
+7.select emoployees hired in last 2 years.
+
+8.select the aveerage salary of employees.  
